9801f5c5c93377898506a6f237259c8e06a33788
Corno Schraverus <corno@schraverus.com>
2025-09-21 08:47:23 +0200

    .

--- src/generated/interface/schemas/boekhouding/data_types/resolve.ts
+++ src/generated/interface/schemas/boekhouding/data_types/resolve.ts
@@ -1,8 +1,8 @@
 import * as _pt from 'exupery-core-types'
 
 import * as _i_resolve from "../../../core/resolve"
-import * as _i_resolved from "./resolved"
-import * as _i_unresolved from "./unresolved"
+import * as _i_resolved from "./source"
+import * as _i_unresolved from "./target"
 
 // **** TYPES
 
