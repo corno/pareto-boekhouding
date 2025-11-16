b9986d230370bf62cec00aada8107dd0b1d572ad
Corno Schraverus <corno@schraverus.com>
2025-11-16 13:46:46 +0100

    mayor rework

--- pub/src/bin/genereer_jaarverslag.ts
+++ pub/src/bin/genereer_jaarverslag.ts
@@ -4,18 +4,15 @@ import * as _eb from 'exupery-core-bin'
 
 import { $$ as procedure } from "../implementation/algorithms/procedures/unguaranteed/genereer_jaarverslag"
 
-import * as d_resources from "../implementation/algorithms/procedures/unguaranteed/genereer_jaarverslag"
-
-_eb.run_unguaranteed_main_procedure<d_resources.Resources>(
+_eb.run_main_procedure(
     ($r) => {
-        return {
+        return procedure({
             'procedures': {
-                'write file': $r.procedures['write file'],
+                'write file': $r.commands['write file'],
             },
             'queries': {
                 'read file': $r.queries['read file']
             }
-        }
+        })
     },
-    procedure
 )
--- pub/src/implementation/algorithms/procedures/unguaranteed/converteer_oude_dataset.ts
+++ pub/src/implementation/algorithms/procedures/unguaranteed/converteer_oude_dataset.ts
@@ -7,6 +7,7 @@ import * as _et from 'exupery-core-types'
 
 import * as d_read_file from "exupery-resources/dist/interface/generated/pareto/schemas/read_file/data_types/source"
 import * as d_write_file from "exupery-resources/dist/interface/generated/pareto/schemas/write_file/data_types/source"
+import * as d_main from "exupery-resources/dist/interface/temp_main"
 
 import { Signature } from "../../../../interface/algorithms/procedures/unguaranteed/genereer_jaarverslag"
 
@@ -19,74 +20,109 @@ const settings = {
 
 export type Resources = {
     'queries': {
-        'read file': _et.Unguaranteed_Query<d_read_file.Parameters, d_read_file.Result, d_read_file.Error, null>
+        'read file': _et.Query<d_read_file.Parameters, d_read_file.Result, d_read_file.Error>
     },
-    'procedures': {
-        'write file': _et.Unguaranteed_Procedure<d_write_file.Parameters, d_write_file.Error, null>
+    'commands': {
+        'write file': _et.Command<d_write_file.Parameters, d_write_file.Error>
     }
 }
 
-export const $$: _et.Unguaranteed_Procedure<_eb.Parameters, _eb.Error, Resources> = ($p, $r) => {
-    return _easync.__create_unguaranteed_procedure({
-        'execute': (on_success, on_error) => {
-            $r.queries['read file'](
-                {
-                    'path': settings['in'],
-                    'escape spaces in path': true
-                },
-                null,
-            ).__start(
-                (file_content) => {
-                    temp_func(file_content).process(
-                        ($) => {
-                            $r.procedures['write file'](
-                                {
-                                    'path': {
-                                        'path': settings['out filename'],
-                                        'escape spaces in path': true,
-                                    },
-                                    'data': $
-                                },
-                                null,
-                            ).__start(
-                                on_success,
-                                ($) => {
-                                    on_error({ 'exit code': 1 })
+export type Procedure = _et.Command_Procedure<d_main.Parameters, d_main.Error, Resources>
 
-                                }
-                            )
-                        },
-                        ($) => {
-                            _ea.cc($, ($) => {
-                                switch ($[0]) {
-                                    case 'parse error': return _ea.ss($, ($) => {
-                                        _ed.log_debug_message(`${$.range.start.relative.line}:${$.range.start.relative.column}-${$.range.end.relative.line}:${$.range.end.relative.column}`, () => { })
-                                        _ea.cc($.type, ($) => {
-                                            switch ($[0]) {
-                                                case 'lexer': return _ea.ss($, ($) => {
-                                                    _ed.log_debug_message(`Lexer error: ${$[0]}`, () => { })
-                                                })
-                                                case 'parser': return _ea.ss($, ($) => {
-                                                    _ed.log_debug_message(`Parser error: ${$.cause[0]}`, () => { })
-                                                })
-                                                default: return _ea.au($[0])
-                                            }
-                                        })
-                                    })
-                                    default: return _ea.au($[0])
-                                }
-                            })
-                            _ed.log_debug_message($[0], () => { })
-                            on_error({ 'exit code': 1 })
-                        }
-                    )
 
+export const $$: Procedure = _easync.create_command_procedure(
+    ($r, $p) => $r.commands['write file'].execute.query(
+        ($) => {
+            _ed.log_debug_message(`failed to write converted dataset to ${settings['out filename']}`, () => { })
+            return ({ 'exit code': 1 })
+        },
+        $r.queries['read file'](
+            {
+                'path': settings['in'],
+                'escape spaces in path': true
+            },
+        ).transform_error(($) => {
+            return { 'exit code': 1 }
+        }).transform_result(($) => {
+            return {
+                'path': {
+                    'path': settings['out filename'],
+                    'escape spaces in path': true,
                 },
-                () => {
-                    _ed.log_debug_message("Could not read input file", () => { })
-                    on_error({ 'exit code': 1 })
-                }
-            )
-        }
-    })
-}
+                'data': $
+            }
+        })
+    )
+)
+
+// {
+'path': {
+    'path': settings['out filename'],
+        'escape spaces in path': true,
+            },
+'data': $
+// },
+
+
+// export const $$_: Procedure = ($r) => {
+//     return ($p) => _easync.__create_procedure_promise({
+//         'execute': (on_success, on_error) => {
+//             $r.queries['read file'](
+//                 {
+//                     'path': settings['in'],
+//                     'escape spaces in path': true
+//                 },
+//             ).__start(
+//                 (file_content) => {
+//                     temp_func(file_content).process(
+//                         ($) => {
+//                             $r.commands['write file'](
+//                                 {
+//                                     'path': {
+//                                         'path': settings['out filename'],
+//                                         'escape spaces in path': true,
+//                                     },
+//                                     'data': $
+//                                 },
+//                             ).__start(
+//                                 on_success,
+//                                 ($) => {
+//                                     on_error({ 'exit code': 1 })
+
+//                                 }
+//                             )
+//                         },
+//                         ($) => {
+//                             _ea.cc($, ($) => {
+//                                 switch ($[0]) {
+//                                     case 'parse error': return _ea.ss($, ($) => {
+//                                         _ed.log_debug_message(`${$.range.start.relative.line}:${$.range.start.relative.column}-${$.range.end.relative.line}:${$.range.end.relative.column}`, () => { })
+//                                         _ea.cc($.type, ($) => {
+//                                             switch ($[0]) {
+//                                                 case 'lexer': return _ea.ss($, ($) => {
+//                                                     _ed.log_debug_message(`Lexer error: ${$[0]}`, () => { })
+//                                                 })
+//                                                 case 'parser': return _ea.ss($, ($) => {
+//                                                     _ed.log_debug_message(`Parser error: ${$.cause[0]}`, () => { })
+//                                                 })
+//                                                 default: return _ea.au($[0])
+//                                             }
+//                                         })
+//                                     })
+//                                     default: return _ea.au($[0])
+//                                 }
+//                             })
+//                             _ed.log_debug_message($[0], () => { })
+//                             on_error({ 'exit code': 1 })
+//                         }
+//                     )
+
+//                 },
+//                 () => {
+//                     _ed.log_debug_message("Could not read input file", () => { })
+//                     on_error({ 'exit code': 1 })
+//                 }
+//             )
+//         }
+//     })
+// }
