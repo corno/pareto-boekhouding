b6f575cce284c29cd7110c39ada0ae4ddbf5b5c2
Corno Schraverus <corno@schraverus.com>
2026-01-06 17:41:43 +0100

    ...

--- pub/src/modules/default_tool_signatures/implementation/manual/command_creators/create_file_to_file_command.ts
+++ pub/src/modules/default_tool_signatures/implementation/manual/command_creators/create_file_to_file_command.ts
@@ -1,29 +1,35 @@
 import * as _p from 'pareto-core-command'
 import * as _pi from 'pareto-core-interface'
+import * as _pdev from 'pareto-core-dev'
 
 import * as signatures from "../../../interface/signatures"
 
 //data types
 import * as d_main from "pareto-resources/dist/interface/to_be_generated/temp_main"
 import * as d_transform_file from "../../../interface/to_be_generated/transform_file"
-import * as d_deserialize_resolved_model from "astn-sealed/dist/interface/to_be_generated/deserialize_resolved_model"
 
 //dependencies
 import * as r_file_in_file_out_from_main from "../schemas/file_in_file_out/refiners/main"
 import * as s_path from "pareto-resources/dist/implementation/manual/schemas/path/serializers"
 import * as s_transform_file from "../schemas/transform_file/serializers"
 
+
 export type Creator = (
     deserializer: _pi.Deserializer_With_Parameters<
         string,
-        d_deserialize_resolved_model.Error,
-        d_deserialize_resolved_model.Parameters
+        string,
+        {
+            'uri': string
+        }
     >,
 ) => signatures.commands.transform_file
 
+
 export const $$: Creator = (deserializer) => _p.command_procedure(($p, $cr, $qr) => [
+
     _p.create_error_handling_context<d_main.Error, d_transform_file.Error>(
         [
+
             _p.refine_without_error_transformation(
                 (abort) => r_file_in_file_out_from_main.Parameters($p, ($) => abort(['file in file out', ['command line arguments', $]])),
                 ($r) => [
@@ -72,6 +78,4 @@ export const $$: Creator = (deserializer) => _p.command_procedure(($p, $cr, $qr)
             'exit code': 1
         },
     ),
-]
-)
-
+])
