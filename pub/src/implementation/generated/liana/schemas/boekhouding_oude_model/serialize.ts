10d8f8dec868e336d94d16f5bc0340d2027eeb64
Corno Schraverus <corno@schraverus.com>
2026-01-26 11:27:27 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/boekhouding_oude_model/serialize.ts
@@ -0,0 +1,10 @@
+
+import * as _p from "pareto-core/dist/serializer"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/boekhouding_oude_model/serialize"
+
+import * as v_serialize from "astn-core/dist/implementation/manual/schemas/sealed_target/serializers"
+
+import * as v_marshall from "./marshall"
+export const Bestandsnaam: t_signatures.Bestandsnaam = ($,) => v_serialize.Document(v_marshall.Bestandsnaam($))
+export const Root: t_signatures.Root = ($,) => v_serialize.Document(v_marshall.Root($))
