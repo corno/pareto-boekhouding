3128bc372f63a4a3f9260ecb0f55e0a45bf08003
Corno Schraverus <corno@schraverus.com>
2026-01-26 20:22:57 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/boekhouding/unmarshall.ts
@@ -0,0 +1,18 @@
+
+import * as _p from "pareto-core/dist/refiner"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/boekhouding/unmarshall"
+export const Fiscaal: t_signatures.Fiscaal = ($,) => _p.unreachable_code_path()
+export const Grootboek_Categorieen: t_signatures.Grootboek_Categorieen = ($,) => _p.unreachable_code_path()
+export const Beheer: t_signatures.Beheer = ($,) => _p.unreachable_code_path()
+export const Grootboekrekeningen: t_signatures.Grootboekrekeningen = ($,) => _p.unreachable_code_path()
+export const Jaarbeheer: t_signatures.Jaarbeheer = ($,) => _p.unreachable_code_path()
+export const Overige_balans_item: t_signatures.Overige_balans_item = ($,) => _p.unreachable_code_path()
+export const Verwijzing_naar_Informele_rekening: t_signatures.Verwijzing_naar_Informele_rekening = ($,) => _p.unreachable_code_path()
+export const Verwijzing_naar_Bankrekening: t_signatures.Verwijzing_naar_Bankrekening = ($,) => _p.unreachable_code_path()
+export const Handelstransacties: t_signatures.Handelstransacties = ($,) => _p.unreachable_code_path()
+export const Mutaties: t_signatures.Mutaties = ($,) => _p.unreachable_code_path()
+export const Jaren: t_signatures.Jaren = ($,) => _p.unreachable_code_path()
+export const Balans_Resultaat_Mutatie: t_signatures.Balans_Resultaat_Mutatie = ($,) => _p.unreachable_code_path()
+export const Eerste_boekjaar: t_signatures.Eerste_boekjaar = ($,) => _p.unreachable_code_path()
+export const Root: t_signatures.Root = ($,) => _p.unreachable_code_path()
---
+++ pub/src/implementation/generated/liana/schemas/boekhouding_oude_model/unmarshall.ts
@@ -0,0 +1,6 @@
+
+import * as _p from "pareto-core/dist/refiner"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/boekhouding_oude_model/unmarshall"
+export const Bestandsnaam: t_signatures.Bestandsnaam = ($,) => _p.unreachable_code_path()
+export const Root: t_signatures.Root = ($,) => _p.unreachable_code_path()
