3665776c2df667df20462eda964de79fcbc05d7d
Corno Schraverus <corno@schraverus.com>
2026-07-05 10:38:35 +0200

    pdt: published version 0.3.22

--- typescript/lib/src/implementation/manual/queries/converteer_oude_dataset.ts
+++ typescript/lib/src/implementation/manual/queries/converteer_oude_dataset.ts
@@ -6,26 +6,26 @@ import * as interface_ from "../../../interface/queries"
 
 //data  types
 import * as d_boekhouding from "../../../interface/generated/liana/schemas/boekhouding/data/resolved"
-import * as d_fp from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
+import * as d_prose from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
 import * as t_path_to_text from "pareto-resources/dist/implementation/manual/transformers/unrestricted_path/text"
 
 //dependencies
 import * as t_fp_to_loc from "pareto-fountain-pen/dist/implementation/manual/transformers/prose/list_of_characters"
 import * as r_boekhouding_oude_model_from_loc from "../../generated/liana/schemas/boekhouding_oude_model/refiners/list_of_characters"
 import * as r_boekhouding_unresolved_from_boekhouding_oude_model from "../refiners/boekhouding_unresolved/boekhouding_oude_model"
-import * as t_deserialize_parse_tree_to_prose from "liana-core/dist/implementation/manual/transformers/deserialize/fountain_pen"
+import * as t_deserialize_parse_tree_to_prose from "liana-core/dist/implementation/manual/transformers/deserialize/prose"
 import * as t_deserialize_parse_tree_to_location from "liana-core/dist/implementation/manual/transformers/deserialize/location"
-import * as t_deserialize_resolve_to_prose from "liana-core/dist/implementation/manual/transformers/resolve/fountain_pen"
+import * as t_deserialize_resolve_to_prose from "liana-core/dist/implementation/manual/transformers/resolve/prose"
 import * as t_deserialize_resolve_to_location from "liana-core/dist/implementation/manual/transformers/resolve/location"
 import * as r_boekhouding_resolved_from_boekhouding_unresolved from "../../generated/liana/schemas/boekhouding/resolved/refiners/unresolved"
 import * as t_boekhouding_resolved_to_prose from "../../generated/liana/schemas/boekhouding/resolved/transformers/fountain_pen"
-import * as t_location_to_prose from "astn-core/dist/implementation/manual/transformers/location/fountain_pen"
+import * as t_location_to_prose from "astn-core/dist/implementation/manual/transformers/location/prose"
 
 //shorthands
 import * as sh from "pareto-fountain-pen/dist/shorthands/prose/deprecated"
 
 export const $$: interface_.query_functions.converteer_oude_dataset = p_.query_function(
-    ($d, $s, $q) => p_super_query_result(p_.e.refine<d_boekhouding.Root_, d_fp.Phrase>(
+    ($d, $s, $q) => p_super_query_result(p_.e.refine<d_boekhouding.Root_, d_prose.Phrase>(
         (abort) => r_boekhouding_resolved_from_boekhouding_unresolved.Root(
             r_boekhouding_unresolved_from_boekhouding_oude_model.Root(
                 r_boekhouding_oude_model_from_loc.Root(
--- typescript/lib/src/implementation/manual/queries/genereer_jaarverslag.ts
+++ typescript/lib/src/implementation/manual/queries/genereer_jaarverslag.ts
@@ -5,16 +5,17 @@ import * as interface_ from "../../../interface/queries"
 
 //data  types
 import * as d_boekhouding from "../../../interface/generated/liana/schemas/boekhouding/data/resolved"
-import * as d_fp from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
+import * as d_prose from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
 
 //dependencies
 import * as r_boekhouding_resolved_from_loc from "../refiners/boekhouding_resolved/list_of_characters"
 import * as t_resolved_to_derived from "../transformers/boekhouding_resolved/derived"
 
-import * as t_deserialize_resolved_to_prose from "liana-core/dist/implementation/manual/transformers/deserialize_resolved/fountain_pen"
+import * as t_deserialize_resolved_to_prose from "liana-core/dist/implementation/manual/transformers/deserialize_resolved/prose"
 import * as t_deserialize_resolved_to_location from "liana-core/dist/implementation/manual/transformers/deserialize_resolved/location"
-import * as t_location_to_prose from "astn-core/dist/implementation/manual/transformers/location/fountain_pen"
-import * as t_html_to_list_of_characters from "pareto-static-html/dist/implementation/manual/transformers/static_html/list_of_characters"
+import * as t_location_to_prose from "astn-core/dist/implementation/manual/transformers/location/prose"
+import * as t_html_to_prose from "pareto-static-html/dist/implementation/manual/transformers/static_html/prose"
+import * as t_prose_to_list_of_characters from "pareto-fountain-pen/dist/implementation/manual/transformers/prose/list_of_characters"
 import * as t_aggregatie_to_jaarverslag_html from "../transformers/derived/jaarverslag_html"
 import * as t_path_to_text from "pareto-resources/dist/implementation/manual/transformers/unrestricted_path/text"
 
@@ -22,36 +23,42 @@ import * as t_path_to_text from "pareto-resources/dist/implementation/manual/tra
 import * as sh from "pareto-fountain-pen/dist/shorthands/prose/deprecated"
 
 export const $$: interface_.query_functions.genereer_jaarverslag = p_.query_function(
-    ($d, $s, $q) => p_super_query_result(p_.e.refine<d_boekhouding.Root_, d_fp.Phrase>(
+    ($d, $s, $q) => p_super_query_result(p_.e.refine<d_boekhouding.Root_, d_prose.Phrase>(
         (abort) => r_boekhouding_resolved_from_loc.Root(
             $d.data,
             ($) => abort(
-sh.ph.composed([
-                sh.ph.literal(t_path_to_text.Node_Path($d.path)),
-                sh.ph.literal(":"),
-                t_location_to_prose.Possible_Range(
-                    t_deserialize_resolved_to_location.Error($),
-                    {
-                        'character location reporting': ['one based', null],
-                    }
-                ),
-                sh.ph.literal(": "),
-                t_deserialize_resolved_to_prose.Error(
-                    $,
-                )
-            ])),
+                sh.ph.composed([
+                    sh.ph.literal(t_path_to_text.Node_Path($d.path)),
+                    sh.ph.literal(":"),
+                    t_location_to_prose.Possible_Range(
+                        t_deserialize_resolved_to_location.Error($),
+                        {
+                            'character location reporting': ['one based', null],
+                        }
+                    ),
+                    sh.ph.literal(": "),
+                    t_deserialize_resolved_to_prose.Error(
+                        $,
+                    )
+                ])),
             {
                 'tab size': $s['tab size']
             }
         )
     )).transform(
         ($) => ({
-            'data': t_html_to_list_of_characters.Document(
-                t_aggregatie_to_jaarverslag_html.Root(
-                    t_resolved_to_derived.Root(
-                        $
+            'data': t_prose_to_list_of_characters.Paragraph(
+                t_html_to_prose.Document(
+                    t_aggregatie_to_jaarverslag_html.Root(
+                        t_resolved_to_derived.Root(
+                            $
+                        )
                     )
-                )
+                ),
+                {
+                    'indentation': "    ",
+                    'newline': "\n",
+                }
             )
         })
     )
