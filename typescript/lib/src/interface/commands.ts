d994b913c4d061399509c8f65340d66a8e4a33b2
Corno Schraverus <corno@schraverus.com>
2026-07-07 16:10:32 +0200

    pdt: ...

--- typescript/lib/src/interface/commands.ts
+++ typescript/lib/src/interface/commands.ts
@@ -1,14 +1,15 @@
 import * as p_ from 'pareto-core/interface/command'
 
 import * as resources_pareto from "pareto-resources/interface/resources"
-import * as resources_pareto_stream from "pareto-stream-api/interface/commands"
+import * as resources_pareto_stream_api from "pareto-stream-api/interface/commands"
+import * as commands_pareto_application_api from "pareto-application-api/interface/resources"
 
 import * as d_serialize_prose from "pareto-fountain-pen/interface/data/prose_serialize"
 
 export namespace procedures {
 
     export type genereer_jaarverslag = p_.Command_Procedure<
-        resources_pareto.resources.commands.main,
+        commands_pareto_application_api.resources.commands.main,
         {
             'tab size': number,
             'serialization parameters': d_serialize_prose.Parameters,
@@ -18,12 +19,12 @@ export namespace procedures {
         },
         {
             'write file': resources_pareto.filesystem_unrestricted.commands.write_file,
-            'log error': resources_pareto_stream.commands.log_error,
+            'log error': resources_pareto_stream_api.commands.log_error,
         }
     >
 
     export type converteer_oude_dataset = p_.Command_Procedure<
-        resources_pareto.resources.commands.main,
+        commands_pareto_application_api.resources.commands.main,
         {
             'tab size': number,
             'serialization parameters': d_serialize_prose.Parameters,
@@ -33,7 +34,7 @@ export namespace procedures {
         },
         {
             'write file': resources_pareto.filesystem_unrestricted.commands.write_file,
-            'log error': resources_pareto_stream.commands.log_error,
+            'log error': resources_pareto_stream_api.commands.log_error,
         }
     >
 
