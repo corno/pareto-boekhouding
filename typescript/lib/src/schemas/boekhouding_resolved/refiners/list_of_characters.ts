352e071f5016ae0970be2d33594fde946df7c798
Corno Schraverus <corno@schraverus.com>
2026-09-05 11:57:20 +0200

    pdt: ...

--- typescript/lib/src/schemas/boekhouding_resolved/refiners/list_of_characters.ts
+++ typescript/lib/src/schemas/boekhouding_resolved/refiners/list_of_characters.ts
@@ -6,12 +6,12 @@ import type * as s_function from "liana-core/modules/resolved_document_deseriali
 import type * as s_out from "../schema.js"
 
 namespace declarations {
-export type Root = p_.Refiner_With_Parameter<
-    s_out.Root,
-    s_function.Error,
-    s_in.List_Of_Characters,
-    s_function.Parameters
->
+    export type Root = p_.Refiner_With_Parameter<
+        s_out.Root,
+        s_function.Error,
+        s_in.List_Of_Characters,
+        s_function.Parameters
+    >
 }
 
 
@@ -20,15 +20,13 @@ export type Root = p_.Refiner_With_Parameter<
 import * as r_boekhouding_resolved_from_boekhouding_unresolved from "../../../modules/boekhouding/schemas/resolved/refiners/unresolved.js"
 import * as r_boekhouding_unresolved_from_loc from "../../../modules/boekhouding/schemas/unresolved/refiners/list_of_characters.js"
 
-export const Root: declarations.Root = ($, abort, $p) => {
-    return r_boekhouding_resolved_from_boekhouding_unresolved.Root(
-        r_boekhouding_unresolved_from_loc.Root(
-            $,
-            ($) => abort(['unresolved document deserialization', $]),
-            $p
-        ),
-        ($) => abort(['resolving', $]),
-        p_.literal.nothing(),
-        p_.literal.nothing(),
-    )
-}
+export const Root: declarations.Root = ($, abort, $p) => r_boekhouding_resolved_from_boekhouding_unresolved.Root(
+    r_boekhouding_unresolved_from_loc.Root(
+        $,
+        ($) => abort(['unresolved document deserialization', $]),
+        $p
+    ),
+    ($) => abort(['resolving', $]),
+    p_.literal.nothing(),
+    p_.literal.nothing(),
+)
